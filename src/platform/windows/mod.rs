2fa84dbdd86cd7df6563662571dfaf325353a080
Sinono3 <aldo@aael.xyz>
2021-07-22 21:13:53 -0400

    windows: Fix initialization error (2)

--- src/platform/windows/mod.rs
+++ src/platform/windows/mod.rs
@@ -46,7 +46,7 @@ impl MediaControls {
             .expect("Windows media controls require an HWND in MediaControlsOptions.");
 
         let controls: SystemMediaTransportControls =
-            unsafe { interop.GetForWindow(HWND(config.hwnd as isize)) }?;
+            unsafe { interop.GetForWindow(HWND(hwnd as isize)) }?;
         let display_updater = controls.DisplayUpdater()?;
 
         Ok(Self {
